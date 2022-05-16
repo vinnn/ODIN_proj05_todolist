4781c4da355c99e4f065ed01cd92babbcef968b3
vinnn <vmoutel@yahoo.fr>
2022-05-16 20:55:20 +0100

    uncommented: devServer: {static: './dist', -- in webpack.config.js to solve rendering pb

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   // devtool: 'inline-source-map',
-  // devServer: {
-    // static: './dist',
-  // },
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
